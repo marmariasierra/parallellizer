226e4fecc4052e992c8077a613e0e726fd6c6d68
marmariasierra <marmaria.sierra@gmail.com>
2018-06-26 15:42:24 +0200

    some validation added. Current directory used when no folder is given as an argument

--- test2_tghi.py
+++ test2_tghi.py
@@ -5,8 +5,12 @@ import os
 import re
 import tempfile
 import threading
+import logging
 
-#from collections import OrderedDict
+# from collections import OrderedDict
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(threadName)s %(message)s')
+logger = logging.getLogger(__name__)
 
 
 class Reader():
@@ -43,31 +47,32 @@ class Reader():
 def get_data(file_name=None, folder=None):
     outputls = []
 
-    folder = folder if folder else ""
+    # folder = folder if folder else ""
+
     if file_name:
-        outputls = os.popen('ghi_ls -le -f ' + file_name).read()
+        outputls = os.popen('ghi_ls -leu -f ' + file_name).read()
         outputls = outputls.split('\n')
+        # catch error if file is not found: "Cannot open 'tt.txt', errno = 2"/"Could not stat 'tt.tx'. Error: 2" (absolute path needed)
 
     elif folder:
-        outputls = os.popen('ghi_ls -le ' + folder).read()
+        outputls = os.popen('ghi_ls -leRu ' + folder).read()
         outputls = outputls.split('\n')
+        # catch error if folder is not found: "Could not stat '/smar'. Error: 2" (absolute path needed)
     else:
-        #outputls = os.popen("ls " + folder).read ()
-        outputls = os.popen('ghi_ls -le ' + folder).read()
+        folder = os.getcwd()
+        print(folder)
+        outputls = os.popen('ghi_ls -leRu ' + folder).read()
         outputls = outputls.split('\n')
-        #add the path tho the current directory to the filename!!# TODO add path to the files
 
     info = {}
-    total_size_data = 0
+    # total_size_data = 0
     for line in outputls:
         # match = re.search (r'(\d+)\s\w{3}\s\d{2}\s(?:\d{2}:\d{2}|\d{4})\s+([^\s]+).+L1-TAPE:([^:]+)', line)
-        # to select only files that are in hpss: use match instead of search??)
-        # match = re.search(r'(^H)\s.+\s(\d+)\s\w{3}\s\d{2}\s(?:\d{2}:\d{2}|\d{4})\s+([^\s]+).+L1-TAPE:([^:]+)', line)
         match = re.match(r'(^H)\s.+\s(\d+)\s\w{3}\s\d{2}\s(?:\d{2}:\d{2}|\d{4})\s+([^\s]+).+L1-TAPE:([^:]+)', line)
 
         if match:
             size = int(match.group(2))
-            total_size_data = total_size_data + size
+            # total_size_data = total_size_data + size
             filename = match.group(3)
             tapename = match.group(4)
 
@@ -76,69 +81,91 @@ def get_data(file_name=None, folder=None):
                 info[tapename]['files'].append(filename)
             else:
                 info[tapename] = {'total_size': size, 'files': [filename]}
-    print(info)
+    # print(info)
     return info
-    #return OrderedDict(sorted(info.items(), key=lambda x: x[1]['total_size'], reverse=True))
+    # return OrderedDict(sorted(info.items(), key=lambda x: x[1]['total_size'], reverse=True))
 
 
-def init_readers(number_readers):
+def init_readers(number_of_readers):
     readers = []
-    for i in range(int(number_readers)):
+    for i in range(int(number_of_readers)):
         readers.append(Reader(i))
     print("{0} Total readers initiated".format(len(readers)))
+    logger.debug("{0} Total readers initiated".format(len(readers)))
     return readers
 
 
 def get_next_reader(readers):
     reader_number = 0
     prev_reader_size = 0
-    # readers = []
 
     for index, reader in enumerate(readers):
+    # for reader_number, reader in enumerate(readers):
         if reader.get_size() < prev_reader_size:
             reader_number = index
+          #  reader_number
         prev_reader_size = reader.get_size()
     return reader_number
 
+def check_reader_result(future):
+    reader = future.result()
+    logger.debug("\nReader: {0} \nOutput: {1}".format(reader.number, reader.cmd_output))
 
-def assign_readers(number_readers, file_name, folder):
-    ordered_info = get_data(file_name, folder)
-    readers = init_readers(number_readers)
 
-    for tape in ordered_info.items():
+def assign_readers(number_of_readers, file_name, folder):
+    # ordered_info = get_data(file_name, folder)
+    info = get_data(file_name, folder)
+    readers = init_readers(number_of_readers)
+
+    # for tape in ordered_info.items():
+    for tape in info.items():
         active_reader = get_next_reader(readers)
         readers[active_reader].add_file_name(tape[1].get('files'))
         readers[active_reader].add_size(tape[1].get('total_size'))
         readers[active_reader].add_tape(tape[0])
-
     return readers
 
 
 def execute_cmd(reader):
     # ssize = float (reader.get_size ()) / 1000000000
     # print("Ssize: %d" % ssize)
-    print("Reader n: {0}, Size: {1}, Tapes: {2}".format(reader.number, reader.get_size(), reader.get_tapes()))
-    print("Files to read: {0}".format(reader.get_file_names()))
+    # print("Reader n: {0}, Size: {1}, Tapes: {2}".format(reader.number, reader.get_size(), reader.get_tapes()))
+    # print("Files to read: {0}".format(reader.get_file_names()))
+    # logger.debug("Ssize: %d" % ssize)
+    logger.debug("Reader n: {0}, Size: {1}, Tapes: {2}".format(reader.number, reader.get_size(), reader.get_tapes()))
+    logger.debug("Files to read: {0}".format(reader.get_file_names()))
 
     with tempfile.NamedTemporaryFile() as tmp:
         filenames = reader.get_file_names()
-        #tmp.write(filenames)
+        # tmp.write(filenames)
         tmp.writelines("%s\n" % f for f in filenames)
-        #tmp.write("\n")
+        # tmp.write("\n")
         tmp.seek(0)
         cmd = 'ghi_stage -v -f ' + tmp.name
-        #cmd = 'ls -l ' + tmp.name
-        #cmd = 'more ' + tmp.name
+        # cmd = 'ghi_stage -v -t 0 -f ' + tmp.name
+        # cmd = 'ls -l ' + tmp.name
+        # cmd = 'more ' + tmp.name
         print(cmd)
-        out=os.system (cmd)
+        out = os.system(cmd)
+
+def validate_parameters(number_readers, file_path, folder):
+    if not isinstance(int(number_readers), int):
+        raise Exception("Number of readers parameter is not correct. Please provide an integer value")
+
+    # if file_path and not os.path.exists(file_path):
+    #     raise Exception("File name provided is not correct or does not exists")
+    #  USE RESULT FROM ghi_ls
+    # if folder and not os.path.isdir(folder):
+    #     raise Exception("Folder provided is not correct or does not exists")
 
 
-def main_process(number_readers, file_name, folder):
+def main_process(number_of_readers, file_name, folder):
     try:
-        readers = assign_readers(number_readers=number_readers, file_name=file_name, folder=folder)
+        # validate_parameters(number_of_readers=number_of_readers, file_name=file_name, folder=folder)
+        readers = assign_readers(number_of_readers=number_of_readers, file_name=file_name, folder=folder)
         threads = []
         for reader in readers:
-            threads.append(threading.Thread(target=execute_cmd, args=(reader,)))
+            threads.append(threading.Thread(target=execute_cmd, args=(reader, )))
         [thread.start() for thread in threads]
 
     except Exception as e:
@@ -149,11 +176,11 @@ def main_process(number_readers, file_name, folder):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="parallel staging")
     parser.add_argument('--readers', type=int, metavar='readers', required=True,
-                        help='number of readers')
+                        help='number of parallel processes to launch. Max XXX')
     parser.add_argument('--file', metavar='file',
-                        help='file containing files to copy')
+                        help='file containing files to stage, absolute path needed')
     parser.add_argument('--folder', metavar='folder',
-                        help='path to directory containing files to copy')
+                        help='absolute path to the directory containing files to stage. Empty for working directory')
     args = parser.parse_args()
-    main_process(number_readers=args.readers, file_name=args.file, folder=args.folder)
 
+    main_process(number_of_readers=args.readers, file_name=args.file, folder=args.folder)
